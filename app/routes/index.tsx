4dbd6d43725fca1b7a27f497a488a0a701dacae2
Spencer Adams <hi@spentaur.com>
2023-01-31 20:57:04 +0000

    mobile search and fixed header

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -34,12 +34,34 @@ export default function Index() {
   return (
     <>
       <div className="h-full bg-gradient-to-t from-gray-100 to-logo-green-300">
-        <div className="flex px-6 pt-4 mx-auto">
-          <div className="basis-1/3 flex h-20 items-center">
-            {/* Search */}
+        <div className="fixed w-full md:relative">
+          <div className="flex pt-2 px-6 md:pt-4 mx-auto">
+            <div className="basis-1/3 flex h-12 md:h-20 items-center">
+              {/* Search */}
+              <button
+                type="button"
+                className="md:inline-flex hidden transition-[padding] ease-in-out duration-500 cursor-text hover:pr-24 items-center rounded-full border border-transparent bg-white pl-4 pr-4 py-2 text-sm font-medium text-logo-green-300 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                <MagnifyingGlassIcon
+                  className="-ml-1 mr-2 h-5 w-5 "
+                  aria-hidden="true"
+                />
+                Search
+              </button>
+            </div>
+            <div className="basis-1/3 h-12 md:h-20 flex justify-center">
+              <img
+                className="transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300 h-12 md:h-20"
+                src="/images/logo-simple.svg"
+                alt="logo"
+              />
+            </div>
+            <div className="basis-1/3 flex h-12 md:h-20 items-center justify-end"></div>
+          </div>
+          <div className="flex w-full py-2 px-6 md:py-6 md:hidden">
             <button
               type="button"
-              className="inline-flex transition-[padding] ease-in-out duration-500 cursor-text hover:pr-24 items-center rounded-full border border-transparent bg-white pl-4 pr-4 py-2 text-sm font-medium text-logo-green-300 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              className="inline-flex cursor-text w-full items-center rounded-full border border-transparent bg-white pl-4 pr-4 py-2 text-sm font-medium text-logo-green-300 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             >
               <MagnifyingGlassIcon
                 className="-ml-1 mr-2 h-5 w-5 "
@@ -48,14 +70,6 @@ export default function Index() {
               Search
             </button>
           </div>
-          <div className="basis-1/3 h-20 flex justify-center">
-            <img
-              className="transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300 h-20"
-              src="/images/logo-simple.svg"
-              alt="logo"
-            />
-          </div>
-          <div className="basis-1/3 flex h-20 items-center justify-end"></div>
         </div>
       </div>
       <div className="h-96">
