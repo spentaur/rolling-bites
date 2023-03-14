fab6bc819c22a019cefb059f47465580740c9cae
Spencer Adams <hi@spentaur.com>
2023-03-14 05:14:40 +0000

    tweeking nav adding cmd k

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -13,6 +13,18 @@ export const Navbar = () => {
   useEffect(() => {
     setOpen(false);
   }, [location]);
+  useEffect(() => {
+    const onKeydown = (e) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen(true);
+      }
+    };
+    window.addEventListener("keydown", onKeydown);
+    return () => {
+      window.removeEventListener("keydown", onKeydown);
+    };
+  }, []);
 
   return (
     <>
@@ -20,7 +32,8 @@ export const Navbar = () => {
       <div className="sticky shadow-sm top-0 z-10 w-full backdrop-blur-sm border-b lg:border-slate-900/10 bg-white/90">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between lg:grid lg:grid-cols-12">
-            <div className="flex items-center lg:col-span-3">
+            <div className="w-10 lg:col-span-3"></div>
+            <div className="flex justify-center items-center lg:col-span-6">
               <Link to="/">
                 <img
                   className="block h-7 lg:h-8 w-auto"
@@ -29,15 +42,18 @@ export const Navbar = () => {
                 />
               </Link>
             </div>
-            <div className="hidden lg:flex justify-center px-6 py-4 min-w-0 flex-1 lg:px-0 lg:col-span-6">
+            <div className="hidden lg:flex justify-center px-6 py-4 min-w-0 flex-1 lg:px-0 lg:col-span-3">
               <button
                 onClick={() => setOpen(true)}
                 type="button"
-                className="max-w-xs w-full flex items-center text-sm leading-6 text-slate-400 sm:rounded-md sm:ring-1 ring-slate-900/20 shadow-sm sm:py-1.5 pl-2 pr-3 py-3 hover:ring-slate-400"
+                className="max-w-xs w-full bg-white/50 backdrop-blur-sm flex items-center text-sm leading-6 text-slate-400 sm:rounded-md sm:ring-1 ring-slate-900/20 shadow-sm sm:py-1.5 pl-2 pr-3 py-3 hover:ring-slate-400"
               >
                 <span className="sr-only">Open search</span>
                 <MagnifyingGlassIcon className="h-5 w-5 mr-3 flex-none" />
                 Quick search...
+                <span className="ml-auto pl-3 flex-none text-xs font-semibold">
+                  ⌘K
+                </span>
               </button>
             </div>
             <div className="flex py-2 items-center lg:hidden">
--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -60,7 +60,7 @@ export const Search = (props: Props) => {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-80 backdrop-blur-sm transition-opacity" />
+          <div className="fixed inset-0 bg-black/20 backdrop-blur-sm transition-opacity" />
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 overflow-y-auto p-4 sm:p-6 md:p-20">
