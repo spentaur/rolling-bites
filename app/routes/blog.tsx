8690f90a30702e30ee9ad1d1a64047122f6366ce
Spencer Adams <hi@spentaur.com>
2023-03-23 02:30:23 +0000

    home page and about page

---
+++ app/routes/blog.tsx
@@ -0,0 +1,132 @@
+import { CheckIcon } from "@heroicons/react/20/solid";
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Example() {
+  return (
+    <>
+      <div className="bg-white">
+        <div className="px-4 sm:px-6 lg:mx-auto lg:max-w-6xl lg:px-8">
+          <h2 className="font-medium">TODO</h2>
+          <ul className="mt-3 space-y-3">
+            <li className="flex items-center">
+              <div className="flex flex-shrink-0">
+                <span className="h-6 w-6 mr-3 flex items-center justify-center bg-green-500 rounded-full">
+                  <CheckIcon className="h-4 w-4 text-white" />
+                </span>
+                Change the things
+              </div>
+            </li>
+            <li className="flex items-center">
+              <div className="flex flex-shrink-0">
+                <span className="h-6 w-6 mr-3 flex items-center justify-center bg-green-500 rounded-full">
+                  <CheckIcon className="h-4 w-4 text-white" />
+                </span>
+                Pastamania menu
+              </div>
+            </li>
+            <li className="flex items-center">
+              <div className="flex flex-shrink-0">
+                <span className="h-6 w-6 mr-3 flex items-center justify-center bg-green-500 rounded-full">
+                  <CheckIcon className="h-4 w-4 text-white" />
+                </span>
+                Pastamania schedule
+              </div>
+            </li>
+            <li className="flex items-center">
+              <div className="flex flex-shrink-0">
+                <span className="h-6 w-6 mr-3 flex items-center justify-center bg-green-500 rounded-full">
+                  <CheckIcon className="h-4 w-4 text-white" />
+                </span>
+                smith burger menu and schedule
+              </div>
+            </li>
+            <li className="flex items-center">
+              <div className="flex flex-shrink-0">
+                <span className="h-6 w-6 mr-3 flex items-center justify-center bg-green-500 rounded-full">
+                  <CheckIcon className="h-4 w-4 text-white" />
+                </span>
+                flour and fire menu and schedule
+              </div>
+            </li>
+            <li className="flex items-center">
+              <div className="flex flex-shrink-0">
+                <span className="h-6 w-6 mr-3 flex items-center justify-center border border-gray-500 bg-transparent rounded-full">
+                  <div className="h-4 w-4" />
+                </span>
+                home page
+              </div>
+            </li>
+            <li className="flex items-center">
+              <div className="flex flex-shrink-0">
+                <span className="h-6 w-6 mr-3 flex items-center justify-center border border-gray-500 bg-transparent rounded-full">
+                  <div className="h-4 w-4" />
+                </span>
+                advanced search
+              </div>
+            </li>
+            <li className="flex items-center">
+              <div className="flex font-bold flex-shrink-0">
+                <span className="h-6 w-6 mr-3 flex items-center justify-center border border-gray-500 bg-transparent rounded-full">
+                  <div className="h-4 w-4" />
+                </span>
+                about,blog,jobs, help,partners, press
+              </div>
+            </li>
+            <li className="flex items-center">
+              <div className="flex font-bold flex-shrink-0">
+                <span className="h-6 w-6 mr-3 flex items-center justify-center border border-gray-500 bg-transparent rounded-full">
+                  <div className="h-4 w-4" />
+                </span>
+                add photos locally
+              </div>
+            </li>
+            <li className="flex items-center">
+              <div className="flex flex-shrink-0">
+                <span className="h-6 w-6 mr-3 flex items-center justify-center border border-gray-500 bg-transparent rounded-full">
+                  <div className="h-4 w-4" />
+                </span>
+                better ads
+              </div>
+            </li>
+            <li className="flex items-center">
+              <div className="flex font-bold flex-shrink-0">
+                <span className="h-6 w-6 mr-3 flex items-center justify-center border border-gray-500 bg-transparent rounded-full">
+                  <div className="h-4 w-4" />
+                </span>
+                add more trucks
+              </div>
+            </li>
+            <li className="flex items-center">
+              <div className="flex flex-shrink-0">
+                <span className="h-6 w-6 mr-3 flex items-center justify-center bg-green-500 rounded-full">
+                  <CheckIcon className="h-4 w-4 text-white" />
+                </span>
+                add api route for siri shortcut to say "where is ~truck~"
+              </div>
+            </li>
+            <li className="flex items-center">
+              <div className="flex flex-shrink-0">
+                <span className="h-6 w-6 mr-3 flex items-center justify-center bg-green-500 rounded-full">
+                  <CheckIcon className="h-4 w-4 text-white" />
+                </span>
+                add "closed now" or "open now" to truck page
+              </div>
+            </li>
+            <li className="flex items-center">
+              <div className="flex flex-shrink-0">
+                <span className="h-6 w-6 mr-3 flex items-center justify-center bg-green-500 rounded-full">
+                  <CheckIcon className="h-4 w-4 text-white" />
+                </span>
+                notication button on profile, make it so modal pops up and add
+                email to list
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,132 +1,120 @@
-import { CheckIcon } from "@heroicons/react/20/solid";
+import { useState } from "react";
+import { Dialog } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
+const navigation = [
+  { name: "Product", href: "#" },
+  { name: "Features", href: "#" },
+  { name: "Marketplace", href: "#" },
+  { name: "Company", href: "#" },
+];
 
 export default function Example() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   return (
-    <>
-      <div className="bg-white">
-        <div className="px-4 sm:px-6 lg:mx-auto lg:max-w-6xl lg:px-8">
-          <h2 className="font-medium">TODO</h2>
-          <ul className="mt-3 space-y-3">
-            <li className="flex items-center">
-              <div className="flex flex-shrink-0">
-                <span className="h-6 w-6 mr-3 flex items-center justify-center bg-green-500 rounded-full">
-                  <CheckIcon className="h-4 w-4 text-white" />
-                </span>
-                Change the things
+    <div className="relative isolate">
+      <div className="absolute top-0 left-1/2 right-0 -z-10 -ml-24 transform-gpu overflow-hidden blur-3xl lg:ml-24 xl:ml-48">
+        <svg
+          viewBox="0 0 801 1036"
+          aria-hidden="true"
+          className="w-[50.0625rem]"
+        >
+          <path
+            fill="url(#70656b7e-db44-4b9b-b7d2-1f06791bed52)"
+            fillOpacity=".3"
+            d="m282.279 843.371 32.285 192.609-313.61-25.32 281.325-167.289-58.145-346.888c94.5 92.652 277.002 213.246 251.009-45.597C442.651 127.331 248.072 10.369 449.268.891c160.956-7.583 301.235 116.434 351.256 179.39L507.001 307.557l270.983 241.04-495.705 294.774Z"
+          />
+          <defs>
+            <linearGradient
+              id="70656b7e-db44-4b9b-b7d2-1f06791bed52"
+              x1="508.179"
+              x2="-28.677"
+              y1="-116.221"
+              y2="1091.63"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#9AC69A" />
+              <stop offset={1} stopColor="#487f48" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
+      <div className="overflow-hidden">
+        <div className="mx-auto max-w-7xl px-6 pb-32 pt-8 lg:px-8">
+          <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
+            <div className="w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
+              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                The food truck connection
+              </h1>
+              <p className="relative mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
+                A delicious way to explore different cuisines and cultures.
+                Whether you crave tacos, burgers, falafel or sushi, you can find
+                it at a food truck near you. Enjoy the convenience and variety
+                of street food with a friendly and casual vibe. You never know
+                what new flavors you might discover at the food truck
+                connection.
+              </p>
+              <div className="mt-10 flex items-center gap-x-6">
+                <a
+                  href="#"
+                  className="rounded-md bg-logo-green-300 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-logo-green-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Explore local trucks
+                </a>
               </div>
-            </li>
-            <li className="flex items-center">
-              <div className="flex flex-shrink-0">
-                <span className="h-6 w-6 mr-3 flex items-center justify-center bg-green-500 rounded-full">
-                  <CheckIcon className="h-4 w-4 text-white" />
-                </span>
-                Pastamania menu
+            </div>
+            <div className="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
+              <div className="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
+                <div className="relative">
+                  <img
+                    src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+                    alt=""
+                    className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                  />
+                  <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+                </div>
               </div>
-            </li>
-            <li className="flex items-center">
-              <div className="flex flex-shrink-0">
-                <span className="h-6 w-6 mr-3 flex items-center justify-center bg-green-500 rounded-full">
-                  <CheckIcon className="h-4 w-4 text-white" />
-                </span>
-                Pastamania schedule
+              <div className="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
+                <div className="relative">
+                  <img
+                    src="https://images.unsplash.com/photo-1485217988980-11786ced9454?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+                    alt=""
+                    className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                  />
+                  <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+                </div>
+                <div className="relative">
+                  <img
+                    src="https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-x=.4&w=396&h=528&q=80"
+                    alt=""
+                    className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                  />
+                  <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+                </div>
               </div>
-            </li>
-            <li className="flex items-center">
-              <div className="flex flex-shrink-0">
-                <span className="h-6 w-6 mr-3 flex items-center justify-center bg-green-500 rounded-full">
-                  <CheckIcon className="h-4 w-4 text-white" />
-                </span>
-                smith burger menu and schedule
+              <div className="w-44 flex-none space-y-8 pt-32 sm:pt-0">
+                <div className="relative">
+                  <img
+                    src="https://images.unsplash.com/photo-1670272504528-790c24957dda?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=left&w=400&h=528&q=80"
+                    alt=""
+                    className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                  />
+                  <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+                </div>
+                <div className="relative">
+                  <img
+                    src="https://images.unsplash.com/photo-1670272505284-8faba1c31f7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+                    alt=""
+                    className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+                  />
+                  <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+                </div>
               </div>
-            </li>
-            <li className="flex items-center">
-              <div className="flex flex-shrink-0">
-                <span className="h-6 w-6 mr-3 flex items-center justify-center bg-green-500 rounded-full">
-                  <CheckIcon className="h-4 w-4 text-white" />
-                </span>
-                flour and fire menu and schedule
-              </div>
-            </li>
-            <li className="flex items-center">
-              <div className="flex flex-shrink-0">
-                <span className="h-6 w-6 mr-3 flex items-center justify-center border border-gray-500 bg-transparent rounded-full">
-                  <div className="h-4 w-4" />
-                </span>
-                home page
-              </div>
-            </li>
-            <li className="flex items-center">
-              <div className="flex flex-shrink-0">
-                <span className="h-6 w-6 mr-3 flex items-center justify-center border border-gray-500 bg-transparent rounded-full">
-                  <div className="h-4 w-4" />
-                </span>
-                advanced search
-              </div>
-            </li>
-            <li className="flex items-center">
-              <div className="flex font-bold flex-shrink-0">
-                <span className="h-6 w-6 mr-3 flex items-center justify-center border border-gray-500 bg-transparent rounded-full">
-                  <div className="h-4 w-4" />
-                </span>
-                about,blog,jobs, help,partners, press
-              </div>
-            </li>
-            <li className="flex items-center">
-              <div className="flex font-bold flex-shrink-0">
-                <span className="h-6 w-6 mr-3 flex items-center justify-center border border-gray-500 bg-transparent rounded-full">
-                  <div className="h-4 w-4" />
-                </span>
-                add photos locally
-              </div>
-            </li>
-            <li className="flex items-center">
-              <div className="flex flex-shrink-0">
-                <span className="h-6 w-6 mr-3 flex items-center justify-center border border-gray-500 bg-transparent rounded-full">
-                  <div className="h-4 w-4" />
-                </span>
-                better ads
-              </div>
-            </li>
-            <li className="flex items-center">
-              <div className="flex font-bold flex-shrink-0">
-                <span className="h-6 w-6 mr-3 flex items-center justify-center border border-gray-500 bg-transparent rounded-full">
-                  <div className="h-4 w-4" />
-                </span>
-                add more trucks
-              </div>
-            </li>
-            <li className="flex items-center">
-              <div className="flex flex-shrink-0">
-                <span className="h-6 w-6 mr-3 flex items-center justify-center bg-green-500 rounded-full">
-                  <CheckIcon className="h-4 w-4 text-white" />
-                </span>
-                add api route for siri shortcut to say "where is ~truck~"
-              </div>
-            </li>
-            <li className="flex items-center">
-              <div className="flex flex-shrink-0">
-                <span className="h-6 w-6 mr-3 flex items-center justify-center bg-green-500 rounded-full">
-                  <CheckIcon className="h-4 w-4 text-white" />
-                </span>
-                add "closed now" or "open now" to truck page
-              </div>
-            </li>
-            <li className="flex items-center">
-              <div className="flex flex-shrink-0">
-                <span className="h-6 w-6 mr-3 flex items-center justify-center bg-green-500 rounded-full">
-                  <CheckIcon className="h-4 w-4 text-white" />
-                </span>
-                notication button on profile, make it so modal pops up and add
-                email to list
-              </div>
-            </li>
-          </ul>
+            </div>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
