2638723ad10e6ae01cff1531b2885590a659873b
Spencer Adams <hi@spentaur.com>
2023-03-24 04:41:25 +0000

    add search button

--- app/routes/__app/search.tsx
+++ app/routes/__app/search.tsx
@@ -109,210 +109,21 @@ export default function Search() {
   return (
     <div className="bg-white">
       <div>
-        {/* Mobile filter dialog */}
-        <Transition.Root show={mobileFiltersOpen} as={Fragment}>
-          <Dialog
-            as="div"
-            className="relative z-40 lg:hidden"
-            onClose={setMobileFiltersOpen}
-          >
-            <Transition.Child
-              as={Fragment}
-              enter="transition-opacity ease-linear duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-opacity ease-linear duration-300"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <div className="fixed inset-0 bg-black bg-opacity-25" />
-            </Transition.Child>
-
-            <div className="fixed inset-0 z-40 flex">
-              <Transition.Child
-                as={Fragment}
-                enter="transition ease-in-out duration-300 transform"
-                enterFrom="translate-x-full"
-                enterTo="translate-x-0"
-                leave="transition ease-in-out duration-300 transform"
-                leaveFrom="translate-x-0"
-                leaveTo="translate-x-full"
-              >
-                <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-12 shadow-xl">
-                  <div className="flex items-center justify-between px-4">
-                    <h2 className="text-lg font-medium text-gray-900">
-                      Filters
-                    </h2>
-                    <button
-                      type="button"
-                      className="-mr-2 flex h-10 w-10 items-center justify-center rounded-md bg-white p-2 text-gray-400"
-                      onClick={() => setMobileFiltersOpen(false)}
-                    >
-                      <span className="sr-only">Close menu</span>
-                      <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                    </button>
-                  </div>
-
-                  {/* Filters */}
-                  <form className="mt-4 border-t border-gray-200">
-                    <h3 className="sr-only">Categories</h3>
-                    <div className="px-2 py-3 text-gray-900">
-                      <label
-                        htmlFor="keywords"
-                        className="block text-sm font-medium leading-6 text-gray-900"
-                      >
-                        Keywords
-                      </label>
-                      <div className="mt-2">
-                        <input
-                          type="text"
-                          name="keywords"
-                          id="keywords"
-                          className="w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 text-sm sm:leading-6"
-                          placeholder="Fried Chicken"
-                        />
-                      </div>
-                    </div>
-                    <div className="px-2 py-3 text-gray-900">
-                      <label
-                        htmlFor="keywords"
-                        className="block text-sm font-medium leading-6 text-gray-900"
-                      >
-                        City
-                      </label>
-                      <div className="mt-2">
-                        <Combobox
-                          value={selected}
-                          onChange={setSelected}
-                          nullable
-                        >
-                          <div className="relative mt-1">
-                            <div className="relative w-full cursor-default overflow-hidden rounded-md text-left">
-                              <Combobox.Input
-                                className="w-full border-none text-left rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 text-sm sm:leading-6 "
-                                onChange={(event) =>
-                                  setQuery(event.target.value)
-                                }
-                                placeholder="Castle Rock, ME"
-                                displayValue={(city) => city?.name}
-                              />
-                              <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-                                <ChevronUpDownIcon
-                                  className="h-5 w-5 text-gray-400"
-                                  aria-hidden="true"
-                                />
-                              </Combobox.Button>
-                            </div>
-                            <Transition
-                              as={Fragment}
-                              leave="transition ease-in duration-100"
-                              leaveFrom="opacity-100"
-                              leaveTo="opacity-0"
-                              afterLeave={() => setQuery("")}
-                            >
-                              <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                                {filteredCities.length === 0 && query !== "" ? (
-                                  <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
-                                    Nothing found.
-                                  </div>
-                                ) : (
-                                  filteredCities.map((city) => (
-                                    <Combobox.Option
-                                      key={city.id}
-                                      className={({ active }) =>
-                                        `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                                          active
-                                            ? "bg-teal-600 text-white"
-                                            : "text-gray-900"
-                                        }`
-                                      }
-                                      value={city}
-                                    >
-                                      {({ selected, active }) => (
-                                        <>
-                                          <span
-                                            className={`block truncate ${
-                                              selected
-                                                ? "font-medium"
-                                                : "font-normal"
-                                            }`}
-                                          >
-                                            {city.name}
-                                          </span>
-                                          {selected ? (
-                                            <span
-                                              className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                                                active
-                                                  ? "text-white"
-                                                  : "text-teal-600"
-                                              }`}
-                                            >
-                                              <CheckIcon
-                                                className="h-5 w-5"
-                                                aria-hidden="true"
-                                              />
-                                            </span>
-                                          ) : null}
-                                        </>
-                                      )}
-                                    </Combobox.Option>
-                                  ))
-                                )}
-                              </Combobox.Options>
-                            </Transition>
-                          </div>
-                        </Combobox>
-                      </div>
-                    </div>
-                    <div className="px-2 py-3 text-gray-900">
-                      <label
-                        htmlFor="keywords"
-                        className="block text-sm font-medium leading-6 text-gray-900"
-                      >
-                        Date
-                      </label>
-                      <div className="mt-2">
-                        <input
-                          type="date"
-                          id="start"
-                          className="w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 text-sm sm:leading-6"
-                          name="trip-start"
-                        />
-                      </div>
-                    </div>
-                  </form>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </Dialog>
-        </Transition.Root>
-
         <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="flex items-baseline justify-between border-b border-gray-200 pt-4 pb-6">
+          <div className="flex items-baseline justify-between py-4">
             <h1 className="text-xl font-bold tracking-tight text-gray-900">
               Advanced Search
             </h1>
-
-            <div className="flex items-center">
-              <button
-                type="button"
-                className="-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden"
-                onClick={() => setMobileFiltersOpen(true)}
-              >
-                <span className="sr-only">Filters</span>
-                <FunnelIcon className="h-5 w-5" aria-hidden="true" />
-              </button>
-            </div>
           </div>
 
-          <section aria-labelledby="products-heading" className="pt-6 pb-24">
+          <section aria-labelledby="products-heading" className="py-6">
             <h2 id="products-heading" className="sr-only">
               Trucks
             </h2>
 
             <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
               {/* Filters */}
-              <form className="hidden lg:block">
+              <form className="">
                 <h3 className="sr-only">Categories</h3>
                 <div>
                   <label
@@ -430,6 +241,16 @@ export default function Search() {
                       className="w-full text-left rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 text-sm sm:leading-6"
                       name="trip-start"
                     />
+                  </div>
+                </div>
+                <div className="pb-6">
+                  <div className="mt-2">
+                    <button
+                      type="submit"
+                      className="rounded-md bg-logo-green-300 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-logo-green-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      Search
+                    </button>
                   </div>
                 </div>
               </form>
